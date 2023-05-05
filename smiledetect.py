51f1d7f641cb38c2cb00e27dc9a999334ebfc0ba
acharyatanu <acharyatanu802@gmail.com>
2023-05-05 10:48:43 +0530

    smiledetect

--- smiledetect.py
+++ smiledetect.py
@@ -8,6 +8,7 @@ fd=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_frontalface_default.
 sd=cv2.CascadeClassifier(cv2.data.haarcascades+'haarcascade_smile.xml')
 vid=cv2.VideoCapture(0)
 notCaptured=True
+seq=0
 while notCaptured:                              #to have infinte loop
     flag,img=vid.read()
     if flag:
@@ -28,9 +29,13 @@ while notCaptured:                              #to have infinte loop
          face=img_gray[y:y+h,x:x+w].copy()
          smiles=sd.detectMultiScale(face,1.1,5,minSize=(50,50))
          if len(smiles)==1:
-          cv2.imwrite('Myselfie.png',img)
-          notCaptured=False
-          break
+             seq+=1
+             if seq==20:
+                 cv2.imwrite ('Myselfie.png',img)
+                 notCaptured=False
+                 break
+         else:
+            seq=0
         
         cv2.rectangle(img,pt1=(x,y),pt2=(x+w,y+h),color=colors[i],thickness=2)
         i+=1
